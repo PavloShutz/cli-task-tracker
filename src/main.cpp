55542bd6ada9f3e0a8b4b897b9168c98ef46a68c
Pavlo Shutz <pavlo050706@gmail.com>
2026-03-03 18:57:04 +0200

    Refactor main.cpp to implement Task struct and JSON serialization/deserialization

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,68 @@
 #include <fstream>
 #include <iostream>
+#include <string>
 
 #include <nlohmann/json.hpp>
 using json = nlohmann::json;
+
+namespace tr
+{
+    static int g_id = 0;
+
+    enum class Status
+    {
+        TODO,
+        IN_PROGRESS,
+        DONE
+    };
+
+    struct Task
+    {
+        int id;
+        Status status;
+        std::string description;
+        std::string createdAt; // yyyy-mm-dd hh:mm:ss
+        std::string updatedAt; // yyyy-mm-dd hh:mm:ss
+    };
+
+    void to_json(json &j, const Task &task)
+    {
+        j = json{
+            {"id", task.id},
+            {"status", task.status},
+            {"description", task.description},
+            {"createdAt", task.createdAt},
+            {"updatedAt", task.updatedAt}};
+    }
+
+    void from_json(const json &j, Task &task)
+    {
+        j.at("id").get_to(task.id);
+        j.at("status").get_to(task.status);
+        j.at("description").get_to(task.description);
+        j.at("createdAt").get_to(task.createdAt);
+        j.at("updatedAt").get_to(task.updatedAt);
+    }
+} // namespace tr
+
+json openJson(const std::string &path)
+{
+    std::ifstream f(path);
+    if (!f.is_open())
+    {
+        std::ofstream ostrm(path);
+        f.open(path);
+    }
+    json j;
+    try
+    {
+        j = json::parse(f);
+    }
+    catch (const json::parse_error &e)
+    {
+    }
+    return j;
+}
 
 void displayHelpInfo()
 {
@@ -19,9 +79,7 @@ int main(int argc, char *argv[])
     }
     else
     {
-        std::ifstream f("test.json");
-        json data = json::parse(f);
-        std::cout << data["pi"] << '\n';
+        json j = openJson("test.json");
     }
     return 0;
 }
