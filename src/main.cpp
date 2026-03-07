b0593a4d122d1828a2af37929292051fdde9d88b
Pavlo Shutz <pavlo050706@gmail.com>
2026-03-07 13:50:42 +0200

    Add help information for update and mark commands in task-cli

--- src/main.cpp
+++ src/main.cpp
@@ -117,6 +117,32 @@ void displayHelpInfo_List()
               << "= Filter tasks by status (todo, in-progress, done). If omitted, all tasks are listed.\n";
 }
 
+void displayHelpInfo_Update()
+{
+    std::cout << "Usage: task-cli update <id> <description>\n\n";
+    std::cout << "Options: \n";
+    std::cout << "  " << std::left << std::setw(30) << "<id>"
+              << "= The ID of the task to update.\n";
+    std::cout << "  " << std::left << std::setw(30) << "<description>"
+              << "= The new description of the task.\n";
+}
+
+void displayHelpInfo_MarkInProgress()
+{
+    std::cout << "Usage: task-cli mark-in-progress <id>\n\n";
+    std::cout << "Options: \n";
+    std::cout << "  " << std::left << std::setw(30) << "<id>"
+              << "= The ID of the task to mark as in-progress.\n";
+}
+
+void displayHelpInfo_MarkDone()
+{
+    std::cout << "Usage: task-cli mark-done <id>\n\n";
+    std::cout << "Options: \n";
+    std::cout << "  " << std::left << std::setw(30) << "<id>"
+              << "= The ID of the task to mark as done.\n";
+}
+
 void displayHelpInfo_Delete()
 {
     std::cout << "Usage: task-cli delete <id>\n\n";
@@ -127,18 +153,39 @@ void displayHelpInfo_Delete()
 
 void displayHelpInfo()
 {
-    // TODO: Output descriptive help man-like page
-    std::cout << "Help command";
+    std::cout << "task-cli - A simple non-overwhelming command-line task tracker\n\n";
+    std::cout << "Usage: task-cli <command> [arguments...]\n\n";
+    std::cout << "Commands:\n";
+    std::cout << "  " << std::left << std::setw(30) << "add <description>"
+              << "Add a new task.\n";
+    std::cout << "  " << std::left << std::setw(30) << "update <id> <description>"
+              << "Update a task's description.\n";
+    std::cout << "  " << std::left << std::setw(30) << "delete <id>"
+              << "Delete a task.\n";
+    std::cout << "  " << std::left << std::setw(30) << "list [status]"
+              << "List tasks, optionally filtered by status.\n";
+    std::cout << "  " << std::left << std::setw(30) << "mark-in-progress <id>"
+              << "Mark a task as in-progress.\n";
+    std::cout << "  " << std::left << std::setw(30) << "mark-done <id>"
+              << "Mark a task as done.\n";
+    std::cout << "  " << std::left << std::setw(30) << "help [command]"
+              << "Show help for a specific command.\n";
 }
 
 void displayHelpInfo(const char *command)
 {
     if (equal(command, "add"))
         displayHelpInfo_Add();
+    else if (equal(command, "update"))
+        displayHelpInfo_Update();
     else if (equal(command, "list"))
         displayHelpInfo_List();
     else if (equal(command, "delete"))
         displayHelpInfo_Delete();
+    else if (equal(command, "mark-in-progress"))
+        displayHelpInfo_MarkInProgress();
+    else if (equal(command, "mark-done"))
+        displayHelpInfo_MarkDone();
     else
         displayHelpInfo();
 }
@@ -163,9 +210,7 @@ void updateTask(int id, const std::string &description, const std::string &file)
 void markTaskStatus(int id, ctt::Status status, const std::string &file)
 {
     auto func = [status](auto &j, auto it)
-    {
-        it.value()["status"] = status;
-    };
+    { it.value()["status"] = status; };
     modifyTask(id, file, func);
 }
 
