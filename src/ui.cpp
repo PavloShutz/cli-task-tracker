05a403847aa449659d5605504606810dba2ff1b2
Pavlo Shutz <pavlo050706@gmail.com>
2026-03-11 15:55:23 +0200

    Refactor help information display to use std::format for improved readability and consistency

--- src/ui.cpp
+++ src/ui.cpp
@@ -1,6 +1,6 @@
 #include "ctt/ui.hpp"
 
-#include <iomanip>
+#include <format>
 #include <iostream>
 
 #include "ctt/utility.hpp"
@@ -11,50 +11,43 @@ void ui::displayHelpInfo_Add()
 {
     std::cout << "Usage: task-cli add <description>\n\n";
     std::cout << "Options: \n";
-    std::cout << "  " << std::left << std::setw(30) << "<description>"
-              << "= A description of the task.\n";
+    std::cout << std::format("  {:<30}= A description of the task.\n", "<description>");
 }
 
 void ui::displayHelpInfo_List()
 {
     std::cout << "Usage: task-cli list [status]\n\n";
     std::cout << "Options: \n";
-    std::cout << "  " << std::left << std::setw(30) << "[status]"
-              << "= Filter tasks by status (todo, in-progress, done). If omitted, all tasks are listed.\n";
+    std::cout << std::format("  {:<30}= Filter tasks by status (todo, in-progress, done). If omitted, all tasks are listed.\n", "[status]");
 }
 
 void ui::displayHelpInfo_Update()
 {
     std::cout << "Usage: task-cli update <id> <description>\n\n";
     std::cout << "Options: \n";
-    std::cout << "  " << std::left << std::setw(30) << "<id>"
-              << "= The ID of the task to update.\n";
-    std::cout << "  " << std::left << std::setw(30) << "<description>"
-              << "= The new description of the task.\n";
+    std::cout << std::format("  {:<30}= The ID of the task to update.\n", "<id>");
+    std::cout << std::format("  {:<30}= The new description of the task.\n", "<description>");
 }
 
 void ui::displayHelpInfo_MarkInProgress()
 {
     std::cout << "Usage: task-cli mark-in-progress <id>\n\n";
     std::cout << "Options: \n";
-    std::cout << "  " << std::left << std::setw(30) << "<id>"
-              << "= The ID of the task to mark as in-progress.\n";
+    std::cout << std::format("  {:<30}= The ID of the task to mark as in-progress.\n", "<id>");
 }
 
 void ui::displayHelpInfo_MarkDone()
 {
     std::cout << "Usage: task-cli mark-done <id>\n\n";
     std::cout << "Options: \n";
-    std::cout << "  " << std::left << std::setw(30) << "<id>"
-              << "= The ID of the task to mark as done.\n";
+    std::cout << std::format("  {:<30}= The ID of the task to mark as done.\n", "<id>");
 }
 
 void ui::displayHelpInfo_Delete()
 {
     std::cout << "Usage: task-cli delete <id>\n\n";
     std::cout << "Options: \n";
-    std::cout << "  " << std::left << std::setw(30) << "<id>"
-              << "= The ID of the task to delete.\n";
+    std::cout << std::format("  {:<30}= The ID of the task to delete.\n", "<id>");
 }
 
 void ui::displayHelpInfo()
@@ -62,20 +55,13 @@ void ui::displayHelpInfo()
     std::cout << "task-cli - A simple non-overwhelming command-line task tracker\n\n";
     std::cout << "Usage: task-cli <command> [arguments...]\n\n";
     std::cout << "Commands:\n";
-    std::cout << "  " << std::left << std::setw(30) << "add <description>"
-              << "Add a new task.\n";
-    std::cout << "  " << std::left << std::setw(30) << "update <id> <description>"
-              << "Update a task's description.\n";
-    std::cout << "  " << std::left << std::setw(30) << "delete <id>"
-              << "Delete a task.\n";
-    std::cout << "  " << std::left << std::setw(30) << "list [status]"
-              << "List tasks, optionally filtered by status.\n";
-    std::cout << "  " << std::left << std::setw(30) << "mark-in-progress <id>"
-              << "Mark a task as in-progress.\n";
-    std::cout << "  " << std::left << std::setw(30) << "mark-done <id>"
-              << "Mark a task as done.\n";
-    std::cout << "  " << std::left << std::setw(30) << "help [command]"
-              << "Show help for a specific command.\n";
+    std::cout << std::format("  {:<30}Add a new task.\n", "add <description>");
+    std::cout << std::format("  {:<30}Update a task's description.\n", "update <id> <description>");
+    std::cout << std::format("  {:<30}Delete a task.\n", "delete <id>");
+    std::cout << std::format("  {:<30}List tasks, optionally filtered by status.\n", "list [status]");
+    std::cout << std::format("  {:<30}Mark a task as in-progress.\n", "mark-in-progress <id>");
+    std::cout << std::format("  {:<30}Mark a task as done.\n", "mark-done <id>");
+    std::cout << std::format("  {:<30}Show help for a specific command.\n", "help [command]");
 }
 
 void ui::displayHelpInfo(const std::string_view command)
